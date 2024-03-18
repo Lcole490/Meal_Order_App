009a86c68d4629351d8df3667fba777f52d7b285
Lcole.engr <lcole.engr@gmail.com>
2024-03-18 13:16:20 -0400

    Created Meals component that fetches meal data from backend and lists information

---
+++ src/components/Meals.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+
+  // The useEffect Hook is implemented here to avoid infinite loop. meals are only fetched once and are not retriggered by component rerender
+  // when fetchMeals function is called.
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+        ///..
+      }
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+
+    fetchMeals();
+  }, []);
+
+
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
