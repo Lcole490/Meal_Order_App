225f7652e34aa11a65581402f10eace7de630608
Lcole.engr <lcole.engr@gmail.com>
2024-03-19 12:37:34 -0400

    Added cartItem component to Cart logic

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 
 
@@ -23,10 +24,9 @@ export default function Cart(){
 
             <h2>Your Cart</h2>
             <ul>
-                {cartCtx.items.map(item => 
-                <li key={item.id}>
-                    {item.name} - {item.quantity}
-                </li>)}
+                {cartCtx.items.map(item => (
+                <CartItem key={item.id} name={item.name} quantity={item.quantity} price={item.price}/>
+                ))}
             </ul>
 
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
