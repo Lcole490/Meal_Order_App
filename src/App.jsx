cd3c5636add836432b4afbc73527808f31cbae17
Lcole.engr <lcole.engr@gmail.com>
2024-03-19 12:17:21 -0400

    Added Cart component to component tree of main app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Meals from './components/Meals';
 import { CartContextProvider } from './store/CartContext';
 import { UserProgressContextProvider } from './store/UserProgressContext';
+import Cart from './components/Cart';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <CartContextProvider>
      <Header />
      <Meals />
+     <Cart />
      </CartContextProvider>
 </UserProgressContextProvider>
 
