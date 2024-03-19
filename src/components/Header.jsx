fda2cc435532ebe5c5e49afc7e8e1a02041b9dd6
Lcole.engr <lcole.engr@gmail.com>
2024-03-19 12:15:43 -0400

    added UserProgresscontext to track user progress and open cart modal when cart button is clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,22 @@ import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button';
 import { useContext } from 'react';
 import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 
 export default function Header(){
 
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity;
     }, 0);
+
+
+    function handleShowCart(){
+        userProgressCtx.showCart();
+    }
 
     return(
         <header id="main-header">
@@ -19,7 +26,7 @@ export default function Header(){
                 <h1>Meals To Order</h1>
             </div>
             <nav>
-                <Button textOnly>Cart ({totalCartItems})</Button>
+                <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
             </nav>
         </header>
     )
