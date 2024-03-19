69b1db8a8d7875949182387b2cca851193d3c0af
Lcole.engr <lcole.engr@gmail.com>
2024-03-19 12:16:52 -0400

    added userProgressCtx to give progress value that tiggers opening of Cart Modal

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,17 +3,19 @@ import { useContext } from "react";
 import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
 
 
 
 export default function Cart(){
 
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0)
 
     return(
-        <Modal className="cart">
+        <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
 
             <h2>Your Cart</h2>
             <ul>
