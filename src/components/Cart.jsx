023915781afae54257f24c7b45fedbe5edbfd2d8
Lcole.engr <lcole.engr@gmail.com>
2024-03-19 12:25:53 -0400

    Added handleCloseCart function to close modal using userProgressCtx.hideCart method

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -12,7 +12,11 @@ export default function Cart(){
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
 
-    const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0)
+    const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0);
+
+    function handleCloseCart(){
+        userProgressCtx.hideCart();
+    }
 
     return(
         <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
@@ -27,8 +31,8 @@ export default function Cart(){
 
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
-                <Button textOnly>Close</Button>
-                <Button> Go To Checkout</Button>
+                <Button textOnly onClick={handleCloseCart}>Close</Button>
+                <Button onClick={handleCloseCart}> Go To Checkout</Button>
             </p>
 
         </Modal>
